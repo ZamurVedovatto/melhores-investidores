de570523ef67582d6deee48af5a2a6e9e4227218
Zamur Vedovatto <vedovatto@gmail.com>
2020-08-15 16:53:03 -0300

    ajustes

--- pages/components/footer/main-footer.js
+++ pages/components/footer/main-footer.js
@@ -1,11 +1,9 @@
 import dynamic from "next/dynamic";
-// const FooterTopComponent = dynamic(() => import("./footer-top"));
 const FooterBottomComponent = dynamic(() => import("./footer-bottom"));
 
 const MainFooter = () => {
   return (
-    <footer id="footer-container" className=" enable_footer_columns_dark">
-      {/* <FooterTop /> */}
+    <footer id="footer-container" className="enable_footer_columns_dark">
       <FooterBottomComponent />
     </footer>
   );
